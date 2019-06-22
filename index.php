eb4c7988231162b79f65a1042ce5c896ffdd8be8
Mufti Alie S <iyakiwan19@gmail.com>
2019-06-22 18:06:30 +0700

    upload file 3

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <html>
 <head>
     <title>Mufti Azure Cloud</title>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
     <style type="text/css">
         body {
             margin: 0;
@@ -20,6 +21,69 @@
 </head>
 
 <body >
+<script type="text/javascript">
+    function processImage() {
+        // **********************************************
+        // *** Update or verify the following values. ***
+        // **********************************************
+ 
+        // Replace <Subscription Key> with your valid subscription key.
+        var subscriptionKey = "24dd32f43e34404f9be7c9c9d5ef7ca6";
+ 
+        // You must use the same Azure region in your REST API method as you used to
+        // get your subscription keys. For example, if you got your subscription keys
+        // from the West US region, replace "westcentralus" in the URL
+        // below with "westus".
+        //
+        // Free trial subscription keys are generated in the "westus" region.
+        // If you use a free trial subscription key, you shouldn't need to change
+        // this region.
+        var uriBase =
+            "https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/analyze";
+ 
+        // Request parameters.
+        var params = {
+            "visualFeatures": "Categories,Description,Color",
+            "details": "",
+            "language": "en",
+        };
+ 
+        // Display the image.
+        var sourceImageUrl = document.getElementById("inputImage").value;
+        document.querySelector("#sourceImage").src = sourceImageUrl;
+ 
+        // Make the REST API call.
+        $.ajax({
+            url: uriBase + "?" + $.param(params),
+ 
+            // Request headers.
+            beforeSend: function(xhrObj){
+                xhrObj.setRequestHeader("Content-Type","application/json");
+                xhrObj.setRequestHeader(
+                    "Ocp-Apim-Subscription-Key", subscriptionKey);
+            },
+ 
+            type: "POST",
+ 
+            // Request body.
+            data: '{"url": ' + '"' + sourceImageUrl + '"}',
+        })
+ 
+        .done(function(data) {
+            // Show formatted JSON on webpage.
+            $("#responseTextArea").val(JSON.stringify(data, null, 2));
+        })
+ 
+        .fail(function(jqXHR, textStatus, errorThrown) {
+            // Display error message.
+            var errorString = (errorThrown === "") ? "Error. " :
+                errorThrown + " (" + jqXHR.status + "): ";
+            errorString += (jqXHR.responseText === "") ? "" :
+                jQuery.parseJSON(jqXHR.responseText).message;
+            alert(errorString);
+        });
+    };
+</script>
     <h1>Website Analisa Gambar</h1>
     <p align="center">by Mufti Alie Satriawan</p>
 
@@ -35,14 +99,10 @@
                 <td colspan="1" align="center"><input name="upload" type="submit" value="Upload Image" /></td>
             </tr>
         </table>
-        
-        <!-- <button type="submit">Press to clean up all resources created by this sample</button> -->
     </form>
 
 
 
-</body>
-</html>
 <?php 
 /**----------------------------------------------------------------------------------
 * Microsoft Developer & Platform Evangelism
@@ -94,7 +154,9 @@ $blobClient = BlobRestProxy::createBlobService($connectionString);
 $fileToUpload;
 $linkurl;
 
-if(isset($_POST["upload"])){
+
+if (isset($_GET["add"])) {
+    if(isset($_POST["upload"])){
 
     $photo= basename($_FILES ['uploadfile']['name']);
     $photo_tmp= $_FILES ['uploadfile']['tmp_name'];
@@ -102,95 +164,111 @@ if(isset($_POST["upload"])){
     $target_file = $target_dir . basename($_FILES["uploadfile"]["name"]);
     if (file_exists($target_file)) {
         echo "Sorry, file already exists.";
-        $uploadOk = 0;
         return;
     }
     if (move_uploaded_file($photo_tmp, $target_file)) {
-        echo "The file ". $photo. " has been uploaded.";
+        //echo "The file ". $photo. " has been uploaded.";
         //echo '<img src="'.$target_file.'" width="400"/>';
         $fileToUpload = $target_file;
+        // Create container options object.
+        $createContainerOptions = new CreateContainerOptions();
+
+        // Set public access policy. Possible values are
+        // PublicAccessType::CONTAINER_AND_BLOBS and PublicAccessType::BLOBS_ONLY.
+        // CONTAINER_AND_BLOBS:
+        // Specifies full public read access for container and blob data.
+        // proxys can enumerate blobs within the container via anonymous
+        // request, but cannot enumerate containers within the storage account.
+        //
+        // BLOBS_ONLY:
+        // Specifies public read access for blobs. Blob data within this
+        // container can be read via anonymous request, but container data is not
+        // available. proxys cannot enumerate blobs within the container via
+        // anonymous request.
+        // If this value is not specified in the request, container data is
+        // private to the account owner.
+        $createContainerOptions->setPublicAccess(PublicAccessType::CONTAINER_AND_BLOBS);
+
+        // Set container metadata.
+        $createContainerOptions->addMetaData("key1", "value1");
+        $createContainerOptions->addMetaData("key2", "value2");
+
+          $containerName = "mufticontainer";
+
+        try {
+            // Create container.
+            //$blobClient->createContainer($containerName, $createContainerOptions);
+
+            // Getting local file so that we can upload it to Azure
+            $myfile = fopen($fileToUpload, "r") or die("Unable to open file!");
+            fclose($myfile);
+            
+            # Upload file as a block blob
+            // echo "Uploading BlockBlob: ".PHP_EOL;
+            // echo $fileToUpload;
+            // echo "<br />";
+            
+            $content = fopen($fileToUpload, "r");
+
+            //Upload blob
+            $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
+
+            // List blobs.
+            $listBlobsOptions = new ListBlobsOptions();
+
+            // echo "These are the blobs present in the container: ";
+            do{
+                $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
+                foreach ($result->getBlobs() as $blob)
+                {
+                    //echo $blob->getName().": ".$blob->getUrl()."<br />";
+                    $linkurl = $blob->getUrl();
+                }
+            
+                $listBlobsOptions->setContinuationToken($result->getContinuationToken());
+            } while($result->getContinuationToken());
+            // echo "<br />";
+
+            //echo $linkurl."<br />";
+            echo '<br><br>Image to analyze:<input type="text" name="inputImage" id="inputImage" value = "'.$linkurl.'" /><button onclick="processImage()">Analyze image</button>';
+
+            
+        }
+        catch(ServiceException $e){
+            // Handle exception based on error codes and messages.
+            // Error codes and messages are here:
+            // http://msdn.microsoft.com/library/azure/dd179439.aspx
+            $code = $e->getCode();
+            $error_message = $e->getMessage();
+            echo $code.": ".$error_message."<br />";
+        }
+        catch(InvalidArgumentTypeException $e){
+            // Handle exception based on error codes and messages.
+            // Error codes and messages are here:
+            // http://msdn.microsoft.com/library/azure/dd179439.aspx
+            $code = $e->getCode();
+            $error_message = $e->getMessage();
+            echo $code.": ".$error_message."<br />";
+        }
     } else {
-        echo "Sorry, there was an error uploading your file.";
+        echo "Fila tidak dapat di upload !! (Ukuran file maximal 2MB)";
     }
 }
-
-if (isset($_GET["add"])) {
-    // Create container options object.
-    $createContainerOptions = new CreateContainerOptions();
-
-    // Set public access policy. Possible values are
-    // PublicAccessType::CONTAINER_AND_BLOBS and PublicAccessType::BLOBS_ONLY.
-    // CONTAINER_AND_BLOBS:
-    // Specifies full public read access for container and blob data.
-    // proxys can enumerate blobs within the container via anonymous
-    // request, but cannot enumerate containers within the storage account.
-    //
-    // BLOBS_ONLY:
-    // Specifies public read access for blobs. Blob data within this
-    // container can be read via anonymous request, but container data is not
-    // available. proxys cannot enumerate blobs within the container via
-    // anonymous request.
-    // If this value is not specified in the request, container data is
-    // private to the account owner.
-    $createContainerOptions->setPublicAccess(PublicAccessType::CONTAINER_AND_BLOBS);
-
-    // Set container metadata.
-    $createContainerOptions->addMetaData("key1", "value1");
-    $createContainerOptions->addMetaData("key2", "value2");
-
-      $containerName = "mufticontainer";
-
-    try {
-        // Create container.
-        $blobClient->createContainer($containerName, $createContainerOptions);
-
-        // Getting local file so that we can upload it to Azure
-        $myfile = fopen($fileToUpload, "r") or die("Unable to open file!");
-        fclose($myfile);
-        
-        # Upload file as a block blob
-        // echo "Uploading BlockBlob: ".PHP_EOL;
-        // echo $fileToUpload;
-        // echo "<br />";
-        
-        $content = fopen($fileToUpload, "r");
-
-        //Upload blob
-        $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
-
-        // List blobs.
-        $listBlobsOptions = new ListBlobsOptions();
-
-        // echo "These are the blobs present in the container: ";
-        do{
-            $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
-            foreach ($result->getBlobs() as $blob)
-            {
-                //echo $blob->getName().": ".$blob->getUrl()."<br />";
-                $linkurl = $blob->getUrl();
-            }
-        
-            $listBlobsOptions->setContinuationToken($result->getContinuationToken());
-        } while($result->getContinuationToken());
-        // echo "<br />";
-
-        echo $linkurl."<br />";
-    }
-    catch(ServiceException $e){
-        // Handle exception based on error codes and messages.
-        // Error codes and messages are here:
-        // http://msdn.microsoft.com/library/azure/dd179439.aspx
-        $code = $e->getCode();
-        $error_message = $e->getMessage();
-        echo $code.": ".$error_message."<br />";
-    }
-    catch(InvalidArgumentTypeException $e){
-        // Handle exception based on error codes and messages.
-        // Error codes and messages are here:
-        // http://msdn.microsoft.com/library/azure/dd179439.aspx
-        $code = $e->getCode();
-        $error_message = $e->getMessage();
-        echo $code.": ".$error_message."<br />";
-    }
-} 
 ?> 
+
+    <br><br>
+    <div id="wrapper" style="width:1020px; display:table;">
+        <div id="imageDiv" style="width:420px; display:table-cell;">
+            Source image:
+            <br><br>
+            <img id="sourceImage" width="400" />
+            <br><br>
+            Response:
+            <br><br>
+            <textarea id="responseTextArea" class="UIInput"
+                      style="width:580px; height:400px;"></textarea>
+        </div>
+    </div>
+
+</body>
+</html>
